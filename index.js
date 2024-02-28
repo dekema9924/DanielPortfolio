7ca56db634aa353a3ac2768b4e67f50361ac8e04
Daniel Unknown <danielunknown@Daniels-MBP.attlocal.net>
2024-02-28 12:24:11 -0500

    completed

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 //NAVBAR
 const hamburger_menu = document.querySelector('.bars');
 const mobile_nav = document.querySelector('.mobile-nav');
+const link_clicked = document.querySelectorAll('.link-clicked');
 
 
 hamburger_menu.addEventListener('click', function(){
     mobile_nav.classList.toggle('menu-open');
     if(mobile_nav.classList.contains("menu-open")){
         console.log("open");
-        // hamburger_menu.classList.add('change-menu')
-        hamburger_menu.src = "./images/x-solid.svg"
+        hamburger_menu.src = "./images/x-solid.svg";
 
     }else{
         console.log(closed)
@@ -18,3 +18,12 @@ hamburger_menu.addEventListener('click', function(){
 
     }
 })
+
+//close navbar after clicking link
+link_clicked.forEach(link => {
+    link.addEventListener('click', ()=>{
+        console.log('link clicked')
+        mobile_nav.classList.remove("menu-open")
+        hamburger_menu.src = "./images/bars-solid.svg"
+    })
+});
